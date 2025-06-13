38fad5b2722a7f4b900347e34d24a1a744dfcf1b
Jace <121627078+Artzkaizen@users.noreply.github.com>
2025-06-13 09:11:40 +0200

    remove pnpm lock

--- packages/design/src/lib/utils.ts
+++ packages/design/src/lib/utils.ts
@@ -6,8 +6,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const splitCamelCaseToWords = (str: string) => {
-  return str
-    .replace(/([A-Z])/g, " $1")
-    .replace(/^./, (str) => str.toUpperCase());
-};
+export const splitCamelCaseToWords = (str: string) =>
+  str.replace(/(?!^)([A-Z])/g, " $1").replace(/^./, (c) => c.toUpperCase());
